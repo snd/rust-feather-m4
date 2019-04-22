ae352e9e5241a5612bc7aa45e69c6b871a08ec8c
Maximilian Krüger <kruemaxi@gmail.com>
2019-04-21 20:36:35 -0500

    make it possible to have multiple binaries you can load on flash

--- flash.sh
+++ flash.sh
@@ -3,4 +3,4 @@
 set -eva
 
 cargo build --release
-bossac --port /dev/cu.usbmodem1421 --offset 0x4000 --erase --write --verify --reset --info --debug target/thumbv7em-none-eabihf/release/rust-feather-m4
+bossac --port /dev/cu.usbmodem1421 --offset 0x4000 --erase --write --verify --reset --info --debug target/thumbv7em-none-eabihf/release/$1
