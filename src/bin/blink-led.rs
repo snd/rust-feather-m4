816c04f55c74b0555eb0ea53c99a539432cd8c8d
Maximilian Krüger <kruemaxi@gmail.com>
2019-04-21 23:00:20 -0500

    blink-led example is almost working

--- src/bin/blink-led.rs
+++ src/bin/blink-led.rs
@@ -1,18 +1,37 @@
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// extern crate panic_abort; // requires nightly
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
-// extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
+extern crate panic_halt;
 
 use cortex_m_rt::entry;
-use atsamd_hal;
+use atsamd_hal::prelude::*;
+use atsamd_hal::delay::Delay;
+use atsamd_hal::clock::GenericClockController;
+use atsamd_hal::hal::digital::OutputPin;
+use atsamd_hal::target_device::{Peripherals, CorePeripherals};
 
 #[entry]
 fn main() -> ! {
+    let mut peripherals = Peripherals::take().unwrap();
+    let port = peripherals.PORT;
+    let mut parts = port.split();
+    // let mut led_pin = parts.pa13.into_open_drain_output(&mut parts.port);
+    let mut led_pin = parts.pa23.into_open_drain_output(&mut parts.port);
+
+    let core = CorePeripherals::take().unwrap();
+    let mut clocks = GenericClockController::with_external_32kosc(
+        peripherals.GCLK,
+        &mut peripherals.MCLK,
+        &mut peripherals.OSC32KCTRL,
+        &mut peripherals.OSCCTRL,
+        &mut peripherals.NVMCTRL,
+    );
+    let mut delay = Delay::new(core.SYST, &mut clocks);
+
     loop {
-        
+        led_pin.set_high();
+        delay.delay_ms(2000u16);
+        led_pin.set_low();
+        delay.delay_ms(2000u16);
     }
 }
